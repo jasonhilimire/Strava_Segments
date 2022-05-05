69e24c9ab7bd56143779977b86c19b3941a9c497
jasonhilimire <jasonhilimire@mac.com>
2022-05-04 22:32:32 -0600

    getting starred segments successfully

--- main.py
+++ main.py
@@ -1,30 +1,58 @@
 from sys import argv
 import requests
 import json
+import secrets
 
 URL = "https://www.strava.com/api/v3/segments/"
-ACCESS_TOKEN = '9ef9aa44d86bfc935078faf191f273a41298ff61'
+STARRED_URL = "https://www.strava.com/api/v3/segments/starred??page=1&per_page=30"
+ACCESS_TOKEN = secrets.TOKEN
 params = {
     "access_token": ACCESS_TOKEN
 }
 
+#TEST Segment = 8087230
 script = argv
-prompt = 'Enter Segment ID> '
-segment_id = input(prompt)
-
-r = requests.get(URL + segment_id, params=params)
-r.raise_for_status()
-segment_data = r.json()
-formatted = json.dumps(segment_data, indent=4, sort_keys=True)
-# print(formatted)
-name = segment_data['name']
-kom = segment_data['xoms']['overall']
-
-athlete_PR_min = (segment_data['athlete_segment_stats']['pr_elapsed_time']) // 60
-athlete_PR_sec = (segment_data['athlete_segment_stats']['pr_elapsed_time']) % 60
-
-print(f"{name} - KOM: {kom} - PR {athlete_PR_min}:{athlete_PR_sec}")
+prompt = 'Enter Segment ID > '
+segment_id = input(prompt).lower()
 
 
-#TODO: if prompt = starred - print 20 starred segments, by NAME, ID, KOM
-# TODO - split overall time and multiply first by 60 to get seconds + seconds - then divid by PR to get % behind
+def get_starred():
+    r = requests.get(STARRED_URL, params=params)
+    r.raise_for_status()
+    starred_data = r.json()
+    formatted = json.dumps(starred_data, indent=4, sort_keys=True)
+    # print(formatted)
+    for data in starred_data:
+        kom = data["athlete_pr_effort"]["is_kom"]
+        pr_min = data["pr_time"] // 60
+        pr_sec = data["pr_time"] % 60
+        if kom:
+            KOM = "👑"
+        else:
+            KOM = ""
+        print(f"{data['id']} - {data['name']} {KOM} - PR = {pr_min}:{pr_sec}")
+
+
+def get_data():
+    r = requests.get(URL + segment_id, params=params)
+    r.raise_for_status()
+    segment_data = r.json()
+    name = segment_data['name']
+    kom = segment_data['xoms']['overall']
+
+    athlete_PR_min = (segment_data['athlete_segment_stats']['pr_elapsed_time']) // 60
+    athlete_PR_sec = (segment_data['athlete_segment_stats']['pr_elapsed_time']) % 60
+
+    print(f"{name} - KOM: {kom} - PR {athlete_PR_min}:{athlete_PR_sec}")
+
+
+if segment_id == 'starred':
+    get_starred()
+else:
+    get_data()
+
+
+# TODO - split overall time and multiply first by 60 to get seconds + seconds - then divide by PR to get % behind
+#TODO abstract into classes
+#TODO get a proper time conversion
+#TODO: TRY STATEMENTs
