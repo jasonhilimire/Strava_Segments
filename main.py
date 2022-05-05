6dd9bf67ff9845578be2f2d737802bce71763bc8
jasonhilimire <jasonhilimire@mac.com>
2022-05-05 07:53:51 -0600

    using datetime for time

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import datetime
 from sys import argv
 import requests
 import json
 import secrets
+
 
 URL = "https://www.strava.com/api/v3/segments/"
 STARRED_URL = "https://www.strava.com/api/v3/segments/starred??page=1&per_page=30"
@@ -20,17 +22,17 @@ def get_starred():
     r = requests.get(STARRED_URL, params=params)
     r.raise_for_status()
     starred_data = r.json()
-    formatted = json.dumps(starred_data, indent=4, sort_keys=True)
+    #formatted = json.dumps(starred_data, indent=4, sort_keys=True)
     # print(formatted)
     for data in starred_data:
         kom = data["athlete_pr_effort"]["is_kom"]
-        pr_min = data["pr_time"] // 60
-        pr_sec = data["pr_time"] % 60
+        pr_time = data["pr_time"]
+        pr_string = str(datetime.timedelta(seconds=pr_time))
         if kom:
             KOM = "👑"
         else:
             KOM = ""
-        print(f"{data['id']} - {data['name']} {KOM} - PR = {pr_min}:{pr_sec}")
+        print(f"{data['id']} - {data['name']} {KOM} - PR = {pr_string}")
 
 
 def get_data():
@@ -39,11 +41,9 @@ def get_data():
     segment_data = r.json()
     name = segment_data['name']
     kom = segment_data['xoms']['overall']
-
-    athlete_PR_min = (segment_data['athlete_segment_stats']['pr_elapsed_time']) // 60
-    athlete_PR_sec = (segment_data['athlete_segment_stats']['pr_elapsed_time']) % 60
-
-    print(f"{name} - KOM: {kom} - PR {athlete_PR_min}:{athlete_PR_sec}")
+    pr_time = segment_data['athlete_segment_stats']['pr_elapsed_time']
+    pr_string = str(datetime.timedelta(seconds=pr_time))
+    print(f"{name} - KOM: {kom} - PR {pr_string}")
 
 
 if segment_id == 'starred':
@@ -54,5 +54,4 @@ else:
 
 # TODO - split overall time and multiply first by 60 to get seconds + seconds - then divide by PR to get % behind
 #TODO abstract into classes
-#TODO get a proper time conversion
 #TODO: TRY STATEMENTs
